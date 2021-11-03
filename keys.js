9f6f4abc87ff195761b1b48a01e79054c7416332
jubin369 <jubinayoob369@gmail.com>
2021-11-03 18:13:56 +0530

    keys for object is solved

--- keys.js
+++ keys.js
@@ -1,1 +1,16 @@
+const testObject = { name: "Bruce Wayne", age: 36, location: "Gotham" };
 
+let result = keys(testObject);
+console.log(result);
+
+function keys(obj) {
+  let keyNames = [];
+
+  for (let k in obj) {
+    keyNames.push(k);
+  }
+  return keyNames;
+  // Retrieve all the names of the object's properties.
+  // Return the keys as strings in an array.
+  // Based on http://underscorejs.org/#keys
+}
