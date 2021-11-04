bcaacb585d7fb7d71f7e03e0075770c6486d0553
jubin369 <jubinayoob369@gmail.com>
2021-11-04 09:35:29 +0530

    default parameter of object is solved

--- defaults.js
+++ defaults.js
@@ -1,1 +1,14 @@
+const testObject = { name: "Bruce Wayne", age: 36, location: "Gotham" };
 
+let result = defaults(testObject, { lastname: "wayne" });
+console.log(result);
+
+function defaults(obj, defaultProps) {
+  for (let k in defaultProps) {
+    obj[k] = defaultProps[k];
+  }
+  return obj;
+  // Fill in undefined properties that match properties on the `defaultProps` parameter object.
+  // Return `obj`.
+  // http://underscorejs.org/#defaults
+}
