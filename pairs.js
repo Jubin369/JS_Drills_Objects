15d021d24b1d89153466a0f89388c8f8c381b895
jubin369 <jubinayoob369@gmail.com>
2021-11-04 09:14:49 +0530

    pairs of object is solved

--- pairs.js
+++ pairs.js
@@ -1,1 +1,15 @@
+const testObject = { name: "Bruce Wayne", age: 36, location: "Gotham" };
 
+let result = pairs(testObject);
+console.log(result);
+
+function pairs(obj) {
+  let arrPairs = [];
+
+  for (let k in obj) {
+    arrPairs.push([k, obj[k]]);
+  }
+  return arrPairs;
+  // Convert an object into a list of [key, value] pairs.
+  // http://underscorejs.org/#pairs
+}
