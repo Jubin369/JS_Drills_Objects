497fc03297052d69647044507d93410ff4f365fb
jubin369 <jubinayoob369@gmail.com>
2021-11-04 09:01:37 +0530

    values of object is solved

--- values.js
+++ values.js
@@ -1,1 +1,16 @@
+const testObject = { name: "Bruce Wayne", age: 36, location: "Gotham" };
 
+let result = values(testObject);
+console.log(result);
+
+function values(obj) {
+  let arrValues = [];
+
+  for (let k in obj) {
+    arrValues.push(obj[k]);
+  }
+  return arrValues;
+  // Return all of the values of the object's own properties.
+  // Ignore functions
+  // http://underscorejs.org/#values
+}
