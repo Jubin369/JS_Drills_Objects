fa3cb596c967b2524afd30ed806315d4b66128cf
jubin369 <jubinayoob369@gmail.com>
2021-11-04 09:22:29 +0530

    mapObject of object is solved

--- mapObject.js
+++ mapObject.js
@@ -1,1 +1,16 @@
+const testObject = { name: "Bruce Wayne", age: 36, location: "Gotham" };
 
+let result = mapObject(testObject);
+console.log(result);
+
+function mapObject(obj, cb) {
+  let data = {};
+
+  for (let k in obj) {
+    data[k] = obj[k] + " map object data added";
+  }
+
+  return data;
+  // Like map for arrays, but for objects. Transform the value of each property in turn by passing it to the callback function.
+  // http://underscorejs.org/#mapObject
+}
