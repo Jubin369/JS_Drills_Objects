ec5d4061f04f81164e684a3f1e7a2495c41f7d0f
jubin369 <jubinayoob369@gmail.com>
2021-11-04 09:27:41 +0530

    invert of object is solved

--- invert.js
+++ invert.js
@@ -1,1 +1,18 @@
+const testObject = { name: "Bruce Wayne", age: 36, location: "Gotham" };
 
+let result = invert(testObject);
+console.log(result);
+
+function invert(obj) {
+  let data = {};
+
+  for (let k in obj) {
+    let tmp = obj[k];
+    data[tmp] = k;
+  }
+
+  return data;
+  // Returns a copy of the object where the keys have become the values and the values the keys.
+  // Assume that all of the object's values will be unique and string serializable.
+  // http://underscorejs.org/#invert
+}
