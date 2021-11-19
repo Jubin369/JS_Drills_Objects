f9b1b8f677fd9c70393a22b5ad8c4c98fb3ffede
jubin369 <jubinayoob369@gmail.com>
2021-11-19 13:26:14 +0530

    mapOject callback function correction

--- test/testMapObject.js
+++ test/testMapObject.js
@@ -1,1 +1,8 @@
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' };
+const key = require('../mapObject');
+
+let answer = key.mapObject(testObject,function(value){
+    return value+" mapped value";
+});
+console.log(answer);
 
